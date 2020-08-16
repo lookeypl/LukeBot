9225146de458f131cc30e841037a5d125ca2ff17
Lukasz Kostyra <costyrra.xl@gmail.com>
2020-08-16 21:38:24 +0200

    LukeBot: Move code to separate LukeBot.cs file

---
+++ LukeBot/LukeBot.cs
@@ -0,0 +1,24 @@
+﻿using LukeBot.Common;
+using LukeBot.Twitch;
+
+namespace LukeBot
+{
+
+class LukeBot
+{
+    private TwitchIRC mTwitch = null;
+    private string mTwitchBotAccount = "lukeboto";
+    private string mTwitchBotChannel = "lookey";
+    private string mTwitchOAuthFile = "Data/oauth_secret.lukebot";
+
+    public void Run()
+    {
+        Logger.Info("LukeBot v0.0.1 starting");
+
+        mTwitch = new TwitchIRC(mTwitchBotAccount, mTwitchBotChannel, mTwitchOAuthFile);
+        mTwitch.Connect();
+        mTwitch.Run();
+    }
+}
+
+}
---
+++ LukeBot/MainClass.cs
@@ -0,0 +1,25 @@
+﻿using LukeBot.Common;
+using System;
+
+namespace LukeBot
+{
+
+class MainClass
+{
+    static void Main(string[] args)
+    {
+        FileUtils.SetUnifiedCWD();
+
+        try
+        {
+            LukeBot bot = new LukeBot();
+            bot.Run();
+        }
+        catch (Exception e)
+        {
+            Logger.Error("Caught exception: " + e.Message + "\n" + e.StackTrace);
+        }
+    }
+}
+
+}

8f096c3816b2f5c80b208193ffd09ad76d791859
Lukasz Kostyra <costyrra.xl@gmail.com>
2020-12-02 16:42:03 +0100

    Create frontend of Add User dialog

--- LukeBot.UI/Pages/js/lukebot.js
+++ LukeBot.UI/Pages/js/lukebot.js
@@ -1,15 +1,50 @@
 const usersURI = "api/Users"
+
+const user = {
+    name: "",
+    displayName: ""
+};
+let usersDropdownMenu = null;
+let addUserUsernameInput = null;
+let addUserDisplayNameInput = null;
 let users = [];
 
 function GetUsers() {
     fetch(usersURI)
         .then(response => response.json())
-        .then(data => _processUsers(data))
+        .then(data => _ProcessUsers(data))
         .catch(error => console.error('Unable to get items.', error));
 }
 
-function _processUsers(usersData) {
+function _ProcessUsers(usersData) {
+    for (const u of users) {
+        _AddUserToDropDownMenu(u);
+    }
+}
 
+function AddUser() {
+    // TODO sanitize
+    let name = addUserUsernameInput.value;
+    let displayName = addUserDisplayNameInput.value;
+    console.log("Adding user " + name + ' with display name ' + displayName);
+    _AddUser(name, displayName);
+}
+
+function _AddUser(name, displayName) {
+    let u = Object.create(user);
+    u.name = addUserUsernameInput.value;
+    u.displayName = addUserDisplayNameInput.value;
+    users.push(u);
+    _AddUserToDropDownMenu(u);
+}
+
+function _AddUserToDropDownMenu(user) {
+    let entry = document.createElement("li");
+    let entryContents = document.createElement("a");
+    entryContents.setAttribute("href", user.name);
+    entryContents.text = user.displayName;
+    entry.appendChild(entryContents);
+    usersDropdownMenu.appendChild(entry);
 }
 
 
@@ -32,6 +67,7 @@ $(document).ready(function () {
                 .then((response) => response.text())
                 .then((html) => {
                     obj.innerHTML = html;
+                    console.log("Included file " + file);
                 })
         );
     });
@@ -47,5 +83,9 @@ $(document).ready(function () {
             } else {
                 Cookies.set(DARK_MODE_COOKIE, 'false');
             }
+
+            usersDropdownMenu = document.getElementById("lukebot-users-dropdown");
+            addUserUsernameInput = document.getElementById("lukebot-adduser-username");
+            addUserDisplayNameInput = document.getElementById("lukebot-adduser-displayname");
         });
 });

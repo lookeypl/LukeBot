0cfa5f165cd6b86a73f4568d1dadee929a497e65
Lookey <costyrra.xl@gmail.com>
2021-08-26 20:41:57 +0200

    Chat widget: Fix words overflowing outside of viewport

--- LukeBot.Common/Constants.cs
+++ LukeBot.Common/Constants.cs
@@ -3,6 +3,6 @@ namespace LukeBot.Common
     public class Constants
     {
         public static string SERVER_IP_FILE = "server_ip";
-        public static string DEFAULT_SERVER_IP = "localhost";
+        public static string DEFAULT_SERVER_IP = "127.0.0.1";
     }
 }

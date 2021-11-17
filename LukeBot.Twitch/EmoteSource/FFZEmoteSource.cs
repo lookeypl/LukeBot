3b4813ed64221242699c09bf6e0aad454ed64b3b
Lookey <costyrra.xl@gmail.com>
2021-11-17 19:59:57 +0100

    Chat: Include FFZ and 7TV global emotes

--- LukeBot.Twitch/EmoteSource/FFZEmoteSource.cs
+++ LukeBot.Twitch/EmoteSource/FFZEmoteSource.cs
@@ -10,9 +10,9 @@ namespace LukeBot.Twitch
 
     class FFZEmoteSource: IEmoteSource
     {
-        private const string FFZ_API_BASE_URI = "https://api.frankerfacez.com/";
-        private const string FFZ_API_VERSION = "v1";
-        private const string FFZ_API_ROOM_URI = FFZ_API_BASE_URI + FFZ_API_VERSION + "/room/id/";
+        private const string FFZ_API_BASE_URI = "https://api.frankerfacez.com/v1/";
+        private const string FFZ_API_GLOBAL_SET_URI  = FFZ_API_BASE_URI + "set/global";
+        private const string FFZ_API_ROOM_URI = FFZ_API_BASE_URI + "room/id/";
 
         private string mUserID;
 
@@ -24,6 +24,32 @@ namespace LukeBot.Twitch
 
         public void FetchEmoteSet(ref Dictionary<string, Emote> emoteSet)
         {
+            // global emotes
+            ResponseJObject globalData = Request.GetJObject(FFZ_API_GLOBAL_SET_URI, null, null);
+            if (globalData.code != HttpStatusCode.OK)
+            {
+                throw new APIErrorException("Failed to fetch FFZ global data: " + globalData.code.ToString());
+            }
+
+            JArray defaultSets = (JArray)globalData.obj["default_sets"];
+            foreach (string globalSetID in defaultSets)
+            {
+                JArray emotes = (JArray)globalData.obj["sets"][globalSetID]["emoticons"];
+                foreach (var e in emotes)
+                {
+                    string name = (string)e["name"];
+                    if (emoteSet.ContainsKey(name))
+                    {
+                        Logger.Log().Warning("FFZ: Skipping duplicate emote name already found in the set: {0}", name);
+                    }
+                    else
+                    {
+                        emoteSet.Add(name, new Emote(EmoteSource.FFZ, name, (string)e["id"], (int)e["width"], (int)e["height"]));
+                    }
+                }
+            }
+
+            // user emotes
             ResponseJObject userData = Request.GetJObject(FFZ_API_ROOM_URI + mUserID, null, null);
             if (userData.code != HttpStatusCode.OK)
             {
--- LukeBot.Twitch/EmoteSource/SevenTVEmoteSource.cs
+++ LukeBot.Twitch/EmoteSource/SevenTVEmoteSource.cs
@@ -10,9 +10,9 @@ namespace LukeBot.Twitch
 
     class SevenTVEmoteSource: IEmoteSource
     {
-        private const string SEVENTV_API_BASE_URI = "https://api.7tv.app/";
-        private const string SEVENTV_API_VERSION = "v2";
-        private const string SEVENTV_API_EMOTES_URI = SEVENTV_API_BASE_URI + SEVENTV_API_VERSION + "/users/";
+        private const string SEVENTV_API_BASE_URI = "https://api.7tv.app/v2/";
+        private const string SEVENTV_API_GLOBAL_EMOTES_URI = SEVENTV_API_BASE_URI + "emotes/global";
+        private const string SEVENTV_API_EMOTES_URI = SEVENTV_API_BASE_URI + "users/";
         private const string SEVENTV_API_EMOTES_URI_TAIL = "/emotes";
 
         private string mUser;
@@ -30,19 +30,40 @@ namespace LukeBot.Twitch
             mUser = user;
         }
 
+        private void AddEmoteDataToSet(ResponseJArray responseObj, ref Dictionary<string, Emote> emoteSet)
+        {
+            foreach (var o in responseObj.array)
+            {
+                string name = (string)o["name"];
+                if (emoteSet.ContainsKey(name))
+                {
+                    Logger.Log().Warning("7TV: Skipping duplicate emote name already found in the set: {0}", name);
+                }
+                else
+                {
+                    emoteSet.Add(name, new Emote(EmoteSource.SevenTV, name, (string)o["id"], (int)o["width"][0], (int)o["height"][0]));
+                }
+            }
+        }
+
         public void FetchEmoteSet(ref Dictionary<string, Emote> emoteSet)
         {
+            // global emotes
+            ResponseJArray globalData = Request.GetJArray(SEVENTV_API_GLOBAL_EMOTES_URI, null, null);
+            if (globalData.code != HttpStatusCode.OK)
+            {
+                throw new APIErrorException("Failed to fetch 7TV global emote data: " + globalData.code.ToString());
+            }
+
+            // user emotes
             ResponseJArray userData = Request.GetJArray(SEVENTV_API_EMOTES_URI + mUser + SEVENTV_API_EMOTES_URI_TAIL, null, null);
             if (userData.code != HttpStatusCode.OK)
             {
                 throw new APIErrorException("Failed to fetch 7TV user data: " + userData.code.ToString());
             }
 
-            foreach (var o in userData.array)
-            {
-                string name = (string)o["name"];
-                emoteSet.Add(name, new Emote(EmoteSource.SevenTV, name, (string)o["id"], (int)o["width"][0], (int)o["height"][0]));
-            }
+            AddEmoteDataToSet(userData, ref emoteSet);
+            AddEmoteDataToSet(globalData, ref emoteSet);
         }
 
         public void GetEmoteInfo()

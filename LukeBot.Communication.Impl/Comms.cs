922d7f9d8023b94e5edec9ca091cec958ab7eb60
Lookey <lookeypl@gmail.com>
2026-01-14 17:55:11 +0100

    Comms: Remove Intercom system
    Intercom System is not used anymore in favor of proper abstraction and
    module APIs

--- LukeBot.Communication.Impl/Comms.cs
+++ LukeBot.Communication.Impl/Comms.cs
@@ -4,7 +4,6 @@
     {
         static private IntermediarySystem mIntermediarySystem;
         static private EventSystem mEventSystem;
-        static private IntercomSystem mIntercomSystem;
         static private bool mInitialized;
 
         static public IntermediarySystem Intermediary
@@ -23,14 +22,6 @@
             }
         }
 
-        static public IntercomSystem Intercom
-        {
-            get
-            {
-                return mIntercomSystem;
-            }
-        }
-
         static public bool Initialized
         {
             get
@@ -45,17 +36,14 @@
                 return;
 
             mEventSystem = new EventSystem();
-            mIntercomSystem = new IntercomSystem();
-
             mIntermediarySystem = new IntermediarySystem();
+
             mInitialized = true;
         }
 
         static public void Teardown()
         {
             mIntermediarySystem = null;
-
-            mIntercomSystem = null;
             mEventSystem = null;
 
             mInitialized = false;
